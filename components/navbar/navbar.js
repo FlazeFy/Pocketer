5f81de1b86fba8fcfcb198a34255ee48a4f32dc3
Leonardho R Sitanggang <79736302+FlazeFy@users.noreply.github.com>
2022-11-29 23:45:59 +0700

    Navbar

---
+++ components/navbar/navbar.js
@@ -0,0 +1,41 @@
+//Font awesome icon
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
+import { } from "@fortawesome/free-regular-svg-icons"
+
+export default function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg w-100">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Pocketer</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Dashboard</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">My Wallet</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">History</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Bucket</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">About</a>
+                        </li>
+                    </ul>
+                    <form className="d-flex">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
+                        <button className="btn btn-outline-success" type="submit"><FontAwesomeIcon icon={faMagnifyingGlass} width="14px"/></button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    )
+}
