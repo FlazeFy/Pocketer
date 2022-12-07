d7c7ecd24c14079c60c2a66bd6b63fdf38a78f3c
Leonardho R Sitanggang <79736302+FlazeFy@users.noreply.github.com>
2022-12-07 21:42:02 +0700

    show highlight total spending

---
+++ pages/api/spending/total.js
@@ -0,0 +1,23 @@
+import mysql from "mysql2/promise";
+
+export default async function handler(req, res) {
+    const dbconnection = await mysql.createConnection({
+        host: "localhost",
+        database: "pocketer",
+        // port: 8889,
+        user: "root",
+    });
+
+    try {
+        const query = "SELECT SUM(purchased_price) as total "+
+            "FROM `purchased`";
+        const values = [];
+        const [data] = await dbconnection.execute(query, values);
+        dbconnection.end();
+
+        //res.status(200).json({ products: data });
+        res.status(200).json({ msg: data.length + " Data retrived", status: 200, data: data})
+    } catch (error) {
+        console.log("Fetch data error");
+    }
+}
--- pages/mainmenu/dashboard/index.js
+++ pages/mainmenu/dashboard/index.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 //Font awesome icon
 import { library } from "@fortawesome/fontawesome-svg-core"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -5,6 +7,29 @@ import { faChartLine, faChevronRight, faDollar, faEllipsisVertical, faMoneyBillT
 import { } from "@fortawesome/free-regular-svg-icons"
 
 export default function Highlight() {
+    //Initial variable
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+
+    //Converter
+    const data = Object.values(items);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/api/spending/total")
+        .then(res => res.json())
+          .then(
+          (result) => {
+            setIsLoaded(true);
+            setItems(result.data);
+          },
+          (error) => {
+            setIsLoaded(true);
+            setError(error);
+          }
+        )
+      },[])
+
     return (
         <>
             <div className="box-highlight">
@@ -14,13 +39,21 @@ export default function Highlight() {
                 <h6 className="box-secondary-text">Last Updated: 28 Nov 22</h6>
                 <button className="btn btn-outline-more box-more" title="See more"><FontAwesomeIcon icon={faChevronRight} width="14px"/></button>
             </div>
-            <div className="box-highlight">
-                <h6><FontAwesomeIcon icon={faDollar} width="13px"/> &nbsp;&nbsp;Spending</h6>
-                <button className="btn btn-transparent box-setting" title="Setting"><FontAwesomeIcon icon={faEllipsisVertical} width="4.5px"/></button>
-                <h5 className="box-main-text">Rp. 1.840.000</h5>
-                <h6 className="box-secondary-text">Last Updated: 28 Nov 22</h6>
-                <button className="btn btn-outline-more box-more" title="See more"><FontAwesomeIcon icon={faChevronRight} width="14px"/></button>
-            </div>
+            {
+                data.map((val, i, index) => {
+
+                    return(
+                        <div className="box-highlight">
+                            <h6><FontAwesomeIcon icon={faDollar} width="13px"/> &nbsp;&nbsp;Spending</h6>
+                            <button className="btn btn-transparent box-setting" title="Setting"><FontAwesomeIcon icon={faEllipsisVertical} width="4.5px"/></button>
+                            <h5 className="box-main-text">Rp. {val.total}</h5>
+                            <h6 className="box-secondary-text">Last Updated: 28 Nov 22</h6>
+                            <button className="btn btn-outline-more box-more" title="See more"><FontAwesomeIcon icon={faChevronRight} width="14px"/></button>
+                        </div>
+                    );
+                })
+            }
+          
             <div className="box-highlight">
                 <h6><FontAwesomeIcon icon={faMoneyBillTrendUp} width="18px"/> &nbsp;&nbsp;Income</h6>
                 <button className="btn btn-transparent box-setting" title="Setting"><FontAwesomeIcon icon={faEllipsisVertical} width="4.5px"/></button>
